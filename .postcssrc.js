0f42d19a1f80a34b00fca68eebc5cef12cb517b2
luxueyan <luxueyan@kaitongamc.com>
2017-05-02 11:46:35 +0800

    modify postcss config

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,22 +1,33 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
-  'parser': 'sugarss',
-  'map': false,
-  'plugins': {
+  parser: 'sugarss',
+  map: false,
+  plugins: {
     // to edit target browsers: use 'browserlist' field in package.json
     // 'autoprefixer': {},
-    'postcss-salad': {
-      'browsers': [
+    postcss - salad: {
+      browsers: [
         '> 1%',
         'last 5 versions',
         'not ie <= 8'
       ],
-      'features': {
-        'bem': true, //pass boolean false can disable the plugin
-        'inlineSvg': {
+      features: {
+        bem: true, //pass boolean false can disable the plugin
+        inlineSvg: {
           'path': 'src/svgs'
         }
+      },
+      separators: {
+        namespace: '--',
+        descendent: '__',
+        modifier: '_'
+      },
+      shortcuts: {
+        utility: 'util',
+        component: 'c',
+        modifier: 'm',
+        descendent: 'd',
+        when: 'w'
       }
     }
   }
